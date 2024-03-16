e76f035b1c8f5602ae5ddd1361ad3ed00919b2c3
JeliKAlex <kisterev0a0a@gmail.com>
2024-03-16 18:52:55 +0700

    implements class Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "Мужской";
+    String name = "Алекс";
+    float height = 183.3f;
+    float weight = 73.5f;
+    int age = 21;
+
+    void walk() {
+        System.out.println("идёт");
+    }
+
+    void sit() {
+        System.out.println("сидит");
+    }
+
+    void run() {
+        System.out.println("бежит");
+    }
+
+    void talk() {
+        System.out.println("говорит");
+    }
+
+    void learnJava() {
+        System.out.println("учит Java");
+    }
+}
