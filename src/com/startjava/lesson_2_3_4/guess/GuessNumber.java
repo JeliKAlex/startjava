6f7b531a9cfff1f9d7d941e83e7736d9ec95043f
JeliKAlex <kisterev0a0a@gmail.com>
2024-05-15 12:36:53 +0700

    rework Player + GuessNumber + RPS

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -104,9 +104,9 @@ public class GuessNumber {
     private static void printNumbs(Player player) {
         System.out.println("\nЧисла игрока " + player.getName() + ":");
         for (int numb : player.getNumbs()) {
-            //if (numb > 0) {
+            if (numb > 0) {
                 System.out.printf("%3d%s", numb, " ");
-            //}
+            }
         }
         System.out.println();
     }
