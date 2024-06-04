79384f660f08f3626c8767de7be7921a96acd1bd
JeliKAlex <kisterev0a0a@gmail.com>
2024-06-04 16:11:58 +0700

    rework HW Lesson_5 populate.sql

--- src/com/startjava/lesson_5/populate.sql
+++ src/com/startjava/lesson_5/populate.sql
@@ -1,11 +1,11 @@
 INSERT INTO Jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill)
-    VALUES  ('Gipsy Danger',    'Mark-3', '79.25', '41.33', 'Destroyed',    'USA',          '1999-02-24', '9'   ),
-            ('Striker Eureka',  'Mark-5', '76.21', '44.76', 'Destroyed',    'China',        '1995-04-12', '11'  ),
-            ('Striker Ebunike', 'Mark-5', '75.22', '43.83', 'Active',       'Australia',    '1997-06-23', '8'   ),
-            ('Cherno Alpha',    'Mark-1', '72.65', '45.75', 'Destroyed',    'Russia',       '1993-04-25', '7'   ),
-            ('Crimson Typhoon', 'Mark-4', '74.55', '47.53', 'Active',       'China',        '1997-08-16', '10'  ),
-            ('Cherno Beta',     'Mark-3', '85.19', '41.77', 'Destroyed',    'USA',          '1998-04-27', '6'   ),
-            ('Gipsy Eureka',    'Mark-4', '76.25', '46.27', 'Active',       'USA',          '1992-12-28', '12'  ),
-            ('Crimson Danger',  'Mark-3', '74.25', '47.78', 'Destroyed',    'Australia',    '1995-05-19', '13'  ),
-            ('Storm Garuda',    'Mark-7', '75.75', '48.52', 'Active',       'Indonesia',    '1993-03-25', '1'   ),
-            ('Gipsy Typhoon',   'Mark-5', '76.95', '44.43', 'Destroyed',    'USA',          '1996-02-14', '7'   );
+    VALUES ('Gipsy Danger',    'Mark-3', '79.25', '41.33', 'Destroyed', 'USA',       '1999-02-24', '9'),
+           ('Striker Eureka',  'Mark-5', '76.21', '44.76', 'Destroyed', 'China',     '1995-04-12', '11'),
+           ('Striker Ebunike', 'Mark-5', '75.22', '43.83', 'Active',    'Australia', '1997-06-23', '8'),
+           ('Cherno Alpha',    'Mark-1', '72.65', '45.75', 'Destroyed', 'Russia',    '1993-04-25', '7'),
+           ('Crimson Typhoon', 'Mark-4', '74.55', '47.53', 'Active',    'China',     '1997-08-16', '10'),
+           ('Cherno Beta',     'Mark-3', '85.19', '41.77', 'Destroyed', 'USA',       '1998-04-27', '6'),
+           ('Gipsy Eureka',    'Mark-4', '76.25', '46.27', 'Active',    'USA',       '1992-12-28', '12'),
+           ('Crimson Danger',  'Mark-3', '74.25', '47.78', 'Destroyed', 'Australia', '1995-05-19', '13'),
+           ('Storm Garuda',    'Mark-7', '75.75', '48.52', 'Active',    'Indonesia', '1993-03-25', '1'),
+           ('Gipsy Typhoon',   'Mark-5', '76.95', '44.43', 'Destroyed', 'USA',       '1996-02-14', '7');
